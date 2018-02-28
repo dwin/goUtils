5ac04600d0ab1e5d168a2c576420817232e142dc
Darwin <dwin@dlsmi.com>
2018-02-28 15:02:35 -0500

    added DirFilenames

--- fileutils/info.go
+++ fileutils/info.go
@@ -1,12 +1,17 @@
 package fileutils
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 )
 
-// DirectorySize takes a directory path, then returns the total size of all files and error
-func DirectorySize(path string) (size int64, err error) {
+var (
+	ErrNotDirectory = errors.New("PathError must give path to directory not file")
+)
+
+// DirSize takes a directory path, then returns the total size of all files and error
+func DirSize(path string) (size int64, err error) {
 	err = filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
 			size += info.Size()
@@ -14,4 +19,21 @@ func DirectorySize(path string) (size int64, err error) {
 		return err
 	})
 	return size, err
+}
+
+func DirFilenames(path string) (info []string, err error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	// If Not Directory return error
+	if !fi.IsDir() {
+		return nil, ErrNotDirectory
+	}
+	defer f.Close()
+	return f.Readdirnames(0)
 }
--- fileutils/info_test.go
+++ fileutils/info_test.go
@@ -1,13 +1,21 @@
 package fileutils
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetDirectorySize(t *testing.T) {
-	size, err := DirectorySize("../.")
+func TestGetDirSize(t *testing.T) {
+	size, err := DirSize("../.")
 	assert.Nil(t, err)
 	assert.NotZero(t, size)
+}
+
+func TestGetDirFilenames(t *testing.T) {
+	files, err := DirFilenames("../.")
+	assert.Nil(t, err)
+	fmt.Println(err)
+	assert.NotEmpty(t, files)
 }
